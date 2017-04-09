320606010759b0b70e17ad38acc082583a1c4af6
Afzal Najam <afzalnaj@gmail.com>
2017-04-09 01:07:31 -0400

    Add types for Desktops and Windows

---
+++ BetterDesktop/BetterDesktop/Desktop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using WindowsDesktop;
+
+namespace BetterDesktop {
+    public class Desktop {
+        private Guid Id;
+        private List<Window> windows;
+        private VirtualDesktop desktop;
+        private UIElement desktopElement;
+    }
+}
---
+++ BetterDesktop/BetterDesktop/Window.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BetterDesktop {
+    public class Window {
+        private IntPtr handle;
+        private string title;
+    }
+}
